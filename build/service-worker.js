c3cedcb8c7a042cd8159136fc9df0ddaf6642385
sandeep-red <45505089+sandeep-red@users.noreply.github.com>
2020-04-01 22:31:52 +0530

    dependency issues

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8a73ae7d2c582cf0671e7fe0f327596.js"
+  "/precache-manifest.7d4e5d58c1873c116e791aee877e0a88.js"
 );
 
 self.addEventListener('message', (event) => {
