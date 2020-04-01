ca637ae9845104302bc9a9a8e29a24b0944fae91
sandeep-red <psandeepreddy060629@gmail.com>
2020-04-02 01:19:45 +0530

    Updates

---
+++ precache-manifest.eb11247e319cbfc1a4c452a3d6cfba13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f2f2c7173443376b9382dd14297cb9",
+    "url": "/corona-virus/index.html"
+  },
+  {
+    "revision": "82d3bce3bd169e873072",
+    "url": "/corona-virus/static/css/2.6db8067b.chunk.css"
+  },
+  {
+    "revision": "bcbee6ff3a9a6d74d9ea",
+    "url": "/corona-virus/static/css/main.6924063f.chunk.css"
+  },
+  {
+    "revision": "82d3bce3bd169e873072",
+    "url": "/corona-virus/static/js/2.92b880ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/corona-virus/static/js/2.92b880ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbee6ff3a9a6d74d9ea",
+    "url": "/corona-virus/static/js/main.c9c5ce0b.chunk.js"
+  },
+  {
+    "revision": "729bd9530f42bf6122bf",
+    "url": "/corona-virus/static/js/runtime-main.6f529eb0.js"
+  }
+]);
